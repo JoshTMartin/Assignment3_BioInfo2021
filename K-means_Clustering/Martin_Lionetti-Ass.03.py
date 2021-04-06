9628da4595bc8f18990afc4a9751aa8eb3a2beb7
LionsFe <lionetti.fe@gmail.com>
2021-04-06 21:38:15 +0200

    Test

--- K-means_Clustering/Martin_Lionetti-Ass.03.py
+++ K-means_Clustering/Martin_Lionetti-Ass.03.py
@@ -2,12 +2,3 @@
 
 import numpy as np
 import pandas as pd
-
-data = pd.read_csv("input.csv",header=None)
-
-print(data)
-n_pivot = round(data[0][0])
-n_samples = round(data[0][1])
-n_groups = round(data[1][1])
-
-
