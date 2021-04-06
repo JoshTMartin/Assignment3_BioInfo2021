4ce7e1856074ab1ec60c8b1cfc29c5c4850953c6
LionsFe <lionetti.fe@gmail.com>
2021-04-06 21:32:01 +0200

    Rename

--- K-means_Clustering/Martin_Lionetti-Ass.03.py
+++ K-means_Clustering/Martin_Lionetti-Ass.03.py
@@ -2,3 +2,12 @@
 
 import numpy as np
 import pandas as pd
+
+data = pd.read_csv("input.csv",header=None)
+
+print(data)
+n_pivot = round(data[0][0])
+n_samples = round(data[0][1])
+n_groups = round(data[1][1])
+
+
