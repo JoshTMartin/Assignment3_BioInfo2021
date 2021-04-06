0b3df5799d86234896d13b50c6dab7351e11ee03
LionsFe <lionetti.fe@gmail.com>
2021-04-06 22:02:36 +0200

    Extracting a single position

--- K-means_Clustering/Martin_Lionetti-Ass.03.py
+++ K-means_Clustering/Martin_Lionetti-Ass.03.py
@@ -2,3 +2,15 @@
 
 import numpy as np
 import pandas as pd
+
+data = pd.read_csv("input.csv", delimiter=';', header=None)  # Importing the data from the CSV file and changing the
+                                                             # delimiter from "," to ";" in order to work them.
+                                                             # Also, we "deleted" the header in order to work with those
+                                                             # data as well.
+
+print(data)
+
+number = (data[0][2].replace(",", ".")) # Example on how to import  a single number for the position of the items
+                                        # in the clusters
+
+print('\n', float(number))
