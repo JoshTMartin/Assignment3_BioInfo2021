8b0be9180b90da340d8e8d02f14a1d40ce3fe64d
LionsFe <lionetti.fe@gmail.com>
2021-04-11 05:12:30 +0200

    2/4 of the project is done

--- K-means_Clustering/Martin_Lionetti-Ass.03.py
+++ K-means_Clustering/Martin_Lionetti-Ass.03.py
@@ -1,41 +1,75 @@
-# Ass. 03 Martin + Lionetti
-import random
-import numpy as np
 import pandas as pd
+import numpy as np
+import random
+from matplotlib import pyplot as plt
 
-data = pd.read_csv("input.csv", delimiter=';', header=None)  # Importing the data from the CSV file and changing the
-# delimiter from "," to ";" in order to work them. # Also, we "deleted" the header in order to work with those data
-# as well.
+while True:
+    data = pd.read_csv("input.csv", delimiter=';', header=None)
+    coordinates = {}
+    name = 0
+    for ii in data[0][2::]:
+        coordinates["item" + str(name)] = [float(ii.replace(",", "."))]
+        name += 1
+    name = 0
+    for xx in data[1][2::]:
+        coordinates["item" + str(name)].append(float(xx.replace(",", ".")))
+        name += 1
 
-go_topless_please = {}
-name = 0
-for ii in data[0][2::]:
-    go_topless_please["item" + str(name)] = [float(ii.replace(",", "."))]
-    name += 1
+    fig= plt.plot(figsize=(5, 5))
 
-name = 0
-for xx in data[1][2::]:
-    go_topless_please["item" + str(name)].append(float(xx.replace(",", ".")))
-    name += 1
+    for cc in range(len(coordinates)):
+        plt.scatter(coordinates['item' + str(cc)][0], coordinates['item' + str(cc)][1], c='black')
+    plt.title("First Run")
+    plt.xlim(0, 1)
+    plt.xticks(np.arange(0, 1.10, 0.10))
+    plt.xlabel("X-axis")
+    plt.ylim(0, 1)
+    plt.yticks(np.arange(0, 1.10, 0.10))
+    plt.ylabel("Y-axis")
+    k1 = 'red'
+    k2 = 'green'
+    k3 = 'blue'
 
-print(go_topless_please)
+    centroid = [random.uniform(min(coordinates[i][0] for i in coordinates.keys()),
+                               max(coordinates[i][0] for i in coordinates.keys())),
+                random.uniform(min(coordinates[i][1] for i in coordinates.keys()),
+                               max(coordinates[i][1] for i in coordinates.keys()))]
+    centroid2 = [random.uniform(min(coordinates[i][0] for i in coordinates.keys()),
+                                max(coordinates[i][0] for i in coordinates.keys())),
+                 random.uniform(min(coordinates[i][1] for i in coordinates.keys()),
+                                max(coordinates[i][1] for i in coordinates.keys()))]
+    centroid3 = [random.uniform(min(coordinates[i][0] for i in coordinates.keys()),
+                                max(coordinates[i][0] for i in coordinates.keys())),
+                 random.uniform(min(coordinates[i][1] for i in coordinates.keys()),
+                                max(coordinates[i][1] for i in coordinates.keys()))]
 
-centers = int(data[0][0])
-
-print("The max and the min of coordinate x are:", max(go_topless_please[i][0] for i in go_topless_please.keys()), ",",
-      min(go_topless_please[i][0] for i in go_topless_please.keys()))
-print("The max and the min of coordinate y are:", max(go_topless_please[i][1] for i in go_topless_please.keys()), ",",
-      min(go_topless_please[i][1] for i in go_topless_please.keys()))
-# RANDOM VALUE FOR THE FIRST POSITION OF THE FIRST CLUSTER-CENTER
-print("Random x coordinate (1st center)",
-      (random.uniform(min(go_topless_please[i][0] for i in go_topless_please.keys()),
-                      max(go_topless_please[i][0] for i in go_topless_please.keys()))))
-print("Random y coordinate (1st center)",
-      (random.uniform(min(go_topless_please[i][1] for i in go_topless_please.keys()),
-                      max(go_topless_please[i][1] for i in go_topless_please.keys()))))
-
-cities = pd.DataFrame([['Sacramento', 'California'], ['Miami', 'Florida']], columns=['Cities', 'Country'])
-
+    plt.scatter(centroid[0], centroid[1], c=k1, marker='*', s=15 * 15)
+    plt.scatter(centroid2[0], centroid2[1], c=k2, marker='*', s=15 * 15)
+    plt.scatter(centroid3[0], centroid3[1], c=k3, marker='*', s=15 * 15)
 
 
+    def manhattan(x, ii):
+        distance = abs(coordinates['item' + str(ii)][0] - x[0]) + abs(coordinates['item' + str(ii)][1] - x[1])
+        return distance
 
+
+    print("Position center1, red:", centroid)
+    print("Position center2, verde:", centroid2)
+    print("Position center3, blue:", centroid3)
+
+
+    def assign(x, y, z):
+        for dd in range(len(coordinates)):
+            i1_center = [manhattan(x, dd), k1]
+            i2_center = [manhattan(y, dd), k2]
+            i3_center = [manhattan(z, dd), k3]
+            close = min(i1_center, i2_center, i3_center)
+            for zzz in (i1_center, i2_center, i3_center):
+                if close == zzz:
+                    plt.scatter(coordinates['item' + str(dd)][0], coordinates['item' + str(dd)][1], c=zzz[1])
+
+
+    assign(centroid, centroid2, centroid3)
+    plt.show()
+
+# TODO NEW CENTROIDS (MEAN OF FOUND ITEMS) AND REALLOCATION
